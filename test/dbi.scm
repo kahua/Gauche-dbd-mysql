8e4fe5986563f8011dc1171c5abbc6d8480d9e95
Tatsuya BIZENN <bizenn@gmail.com>
2007-02-16 07:43:43 +0000

    Comment fix.
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Gauche-dbd-mysql/trunk@2548 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- test/dbi.scm
+++ test/dbi.scm
@@ -1,6 +1,11 @@
-;;;
-;;; Test dbd.mysql
-;;;
+;; -*- mode: scheme; coding: utf-8 -*-
+;;
+;; Test for dbd.mysql via DBI.
+;;
+;;  Copyright (c) 2003-2007 Scheme Arts, L.L.C., All rights reserved.
+;;  Copyright (c) 2003-2007 Time Intermedia Corporation, All rights reserved.
+;;
+;; $Id: dbi.scm,v 1.2 2007/02/16 07:43:43 bizenn Exp $
 
 (use gauche.test)
 (use gauche.collection)
