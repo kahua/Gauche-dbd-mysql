ab684a447644ea0eaafa9f3578deb21d7cf70b24
Tatsuya BIZENN <bizenn@gmail.com>
2009-11-27 03:45:21 +0000

    Add some tests for no-db connection.
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Gauche-dbd-mysql/trunk@3547 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- test/dbd.scm
+++ test/dbd.scm
@@ -22,6 +22,24 @@
 (define *stmt* #f)
 
 (test-section "Connection")
+
+(define (is-class? class obj)
+  (eq? class (class-of obj)))
+
+(test* "mysql-get-client-info" <string> (mysql-get-client-info) is-class?)
+(test* "mysql-get-client-version" <integer> (mysql-get-client-version) is-class?)
+
+;; Connection with default user/no password/no db
+(test* "mysql-real-connect/default user/no password/no db" <mysql-handle>
+       (let1 c (mysql-real-connect #f #f #f #f 0 #f 0)
+	 (set! *mysql* c)
+	 c)
+	 is-class?)
+(test* "mysql-get-server-info" <string> (mysql-get-server-info *mysql*) is-class?)
+(test* "mysql-get-server-version" <integer> (mysql-get-server-version *mysql*) is-class?)
+(test* "mysql-get-host-info" <string> (mysql-get-host-info *mysql*) is-class?)
+(test* "mysql-get-proto-info" <integer> (mysql-get-proto-info *mysql*) is-class?)
+
 (test* "mysql-real-connect/fail" (test-error <mysql-error>)
        (mysql-real-connect #f "" "" "nonexistent" 0 #f 0))
 (test* "mysql-real-connect/success" <mysql-handle>
@@ -96,9 +114,6 @@
       (binary? . #f) (auto-increment? . #f) (decimals . 0)
       (charset-number . 33) (type . ,MYSQL_TYPE_VAR_STRING)
       )))
-
-(define (is-class? class obj)
-  (eq? class (class-of obj)))
 
 (define (check-field info field)
   (define (cmp-slot-value slot-name cmp-func?)
