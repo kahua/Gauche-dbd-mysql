180cd47a2674dd07fb10fba5395b0d1fd5294b74
Shiro Kawai <shiro@acm.org>
2022-04-28 21:32:20 -1000

    Add automated test

--- tools/create-test-db-and-user.sql
+++ tools/create-test-db-and-user.sql
@@ -1,3 +1,3 @@
-create database kahua_test default character set utf8;
-grant all privileges on kahua_test.* to 'kahua_test'@'localhost'
-      identified by 'kahua_secret';;
+create user 'kahua_test'@'localhost' identified by 'kahua_secret';
+create database kahua_test default character set 'utf8';
+grant all on kahua_test.* to 'kahua_test'@'localhost';
